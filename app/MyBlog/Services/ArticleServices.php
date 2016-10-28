7df664b6d1ba71ba55d33dffc653314d35b43f14
suhanyujie <suhanyujie@qq.com>
2016-10-28 11:55:31 +0800

    add 'Article Services' Class

---
+++ app/MyBlog/Services/ArticleServices.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhanyu
+ * Date: 16/10/28
+ * Time: 上午11:51
+ */
+
+namespace MyBlog\Services;
+
+
+class ArticleServices extends BaseServices
+{
+
+}
---
+++ app/MyBlog/Services/BaseServices.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhanyu
+ * Date: 16/10/28
+ * Time: 上午11:53
+ */
+
+namespace MyBlog\Services;
+
+
+use App\Http\Controllers\Controller;
+
+class BaseServices extends Controller
+{
+
+}
