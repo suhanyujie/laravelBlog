ce48b63aa9376bb775102d5605f3163c566aef2a
suhanyujie <suhanyujie@qq.com>
2018-03-05 09:51:54 +0800

    添加廖雪峰链接

--- resources/views/articles/nav.blade.php
+++ resources/views/articles/nav.blade.php
@@ -102,6 +102,7 @@
                 <ul class="dropdown-menu">
                     <li>
                         <a href="https://cuiqingcai.com/">崔庆才的个人博客</a>
+                        <a href="https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000">廖雪峰Python教程</a>
                     </li>
                 </ul>
             </li>
