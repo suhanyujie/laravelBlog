a74277d4a1b9ac1ac8acb779fb165a640e441492
suhanyujie <suhanyujie@qq.com>
2018-05-28 08:53:00 +0800

    调整一下footer 加入自己的网名

--- resources/views/articles/footer.blade.php
+++ resources/views/articles/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer copyright">
     <div class="footer-inner">
         <div class="copyright pull-left">
-            &copy; 爱生活-锲而不舍-suhanyujie@qq.com
+            &copy; 爱生活-锲而不舍-suhanyujie@qq.com | 亡命之徒
         </div>
         <div class="trackcode pull-right">
 
